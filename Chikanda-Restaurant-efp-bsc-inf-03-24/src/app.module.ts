7a2142e5454a5c1b3ffae50d00d3a406dd05973a
efp-bsc-inf-03-24 <efp-bsc-inf-03-24@unima.ac.mw>
2026-05-10 15:20:41 +0200

    Swagger

--- Chikanda-Restaurant-efp-bsc-inf-03-24/src/app.module.ts
+++ Chikanda-Restaurant-efp-bsc-inf-03-24/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { MenuModule } from './menu/menu.module';
 import { OrdersModule } from './orders/orders.module';
 import { ReservationsModule } from './reservations/reservations.module';
@@ -10,8 +12,26 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    
-    MenuModule, OrdersModule, ReservationsModule, PaymentsModule, UsersModule],
+    TypeOrmModule.forRoot({
+      type: 'oracle',
+      host: 'localhost',
+      port: 1521,
+      username: 'pdbadmin',
+      password: 'StrongPassword123',
+      connectString: '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=mypdb)))',
+
+      // ✅ FIX: automatically loads ALL entities (Order included)
+      autoLoadEntities: true,
+
+      synchronize: true,
+    }),
+
+    MenuModule,
+    OrdersModule,
+    ReservationsModule,
+    PaymentsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
